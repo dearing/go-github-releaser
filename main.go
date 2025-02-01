85b6eeef77e0214772c972d8c85bf26adadc2aee
Jacob Dearing <jacob.dearing@gmail.com>
2025-02-01 07:10:18 +0000

    cleanup

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"archive/zip"
 	"bufio"
 	"context"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -15,7 +22,7 @@ import (
 	"github.com/google/go-github/v68/github"
 )
 
-var version = "0.1.0"
+var version = "0.2.0"
 
 // csv file build-matrix
 var csvFile = flag.String("csv-file", "go-github-releaser.csv", "csv file with build information")
@@ -32,11 +39,17 @@ var sumSHA256 = flag.Bool("sum-sha256", false, "create sha256 sum file")
 // produce a zip file for each binary
 var zipFile = flag.Bool("zip", false, "create zip file")
 
+// skip because we already built the binaries
+var skipBuild = flag.Bool("skip-build", false, "skip the build step")
+
+// cut a github release
 var cutRelease = flag.Bool("cut-release", false, "cut a github release")
 
+// github information
 var githubOwner = flag.String("github-owner", "", "github owner")
 var githubRepos = flag.String("github-repos", "", "github repos")
 
+// release information
 var releaseTag = flag.String("release-tag", "", "github release tag")
 var releaseName = flag.String("release-name", "", "github release name")
 var releaseMessage = flag.String("release-message", "", "github release message")
@@ -44,18 +57,8 @@ var releaseDraft = flag.Bool("release-draft", true, "github release draft")
 var releasePrerelease = flag.Bool("release-prerelease", false, "github release prerelease")
 var releaseCommitish = flag.String("release-commitish", "", "github release commitish")
 
-var skipBuild = flag.Bool("skip-build", false, "skip the build step")
-
-func usage() {
-	fmt.Println("go-github-releaser is a tool to build go binaries for multiple platforms and create checksums and zip files")
-	fmt.Fprintf(os.Stderr, "usage: go-github-releaser [flags]\n")
-	flag.PrintDefaults()
-	os.Exit(2)
-}
-
 func main() {
 
-	flag.Usage = usage
 	flag.Parse()
 
 	fmt.Printf("github.com/dearing/go-github-releaser version %s\n", version)
@@ -107,6 +110,213 @@ func main() {
 		log.Printf("cut operation took %s\n", time.Since(start))
 	}
 
+}
+
+func do(goOS, goARCH, target string) error {
+
+	log.Printf("building %s/%s %s\n", goOS, goARCH, target)
+	err := doBuild(goOS, goARCH, target)
+	if err != nil {
+		return fmt.Errorf("building: %s: %v", target, err)
+	}
+
+	if *sumMD5 {
+		log.Printf("  creating md5sum for %s\n", target)
+		err = doMD5(target)
+		if err != nil {
+			return fmt.Errorf("creating md5: %s: %v", target, err)
+		}
+	}
+
+	if *sumSHA1 {
+		log.Printf("  creating sha1sum for %s\n", target)
+		err = doSHA1(target)
+		if err != nil {
+			return fmt.Errorf("creating sha1: %s: %v", target, err)
+		}
+	}
+
+	if *sumSHA256 {
+		log.Printf("  creating sha256sum for %s\n", target)
+		err = doSHA256(target)
+		if err != nil {
+			return fmt.Errorf("creating sha256: %s: %v", target, err)
+		}
+	}
+
+	if *zipFile {
+		log.Printf("  creating zip for %s\n", target)
+		err = doZip(target)
+		if err != nil {
+			return fmt.Errorf("creating zip: %s: %v", target, err)
+		}
+	}
+	return nil
+}
+
+func doBuild(goOS, goARCH, target string) error {
+
+	cmd := exec.Command("go", "build", "-o", target, *srcDir)
+
+	// set environment variables
+	cmd.Env = append(os.Environ(),
+		"GOOS="+goOS,
+		"GOARCH="+goARCH,
+	)
+
+	// Save the original stdout and stderr
+	origStdout := os.Stdout
+	origStderr := os.Stderr
+
+	// Restore stdout and stderr after completion
+	defer func() {
+		os.Stdout = origStdout
+		os.Stderr = origStderr
+	}()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func doMD5(target string) error {
+	file, err := os.Open(target)
+	if err != nil {
+		return errors.New("error opening file")
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return errors.New("error hashing file")
+	}
+
+	// Remove the build directory prefix from the target
+	relativeTarget, err := filepath.Rel(*outDir, target)
+	if err != nil {
+		return errors.New("error getting relative path")
+	}
+
+	sum := hash.Sum(nil)
+
+	// write the sum to a file
+	sumFile, err := os.Create(target + ".md5.txt")
+	if err != nil {
+		return errors.New("error creating sum file")
+	}
+	defer sumFile.Close()
+
+	content := fmt.Sprintf("%x  %s\n", sum, relativeTarget)
+	_, err = sumFile.WriteString(content)
+	if err != nil {
+		return errors.New("error writing sum file")
+	}
+
+	return nil
+}
+
+func doSHA1(target string) error {
+	file, err := os.Open(target)
+	if err != nil {
+		return errors.New("error opening file")
+	}
+	defer file.Close()
+
+	hash := sha1.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return errors.New("error hashing file")
+	}
+
+	// Remove the build directory prefix from the target
+	relativeTarget, err := filepath.Rel(*outDir, target)
+	if err != nil {
+		return errors.New("error getting relative path")
+	}
+
+	sum := hash.Sum(nil)
+
+	// write the sum to a file
+	sumFile, err := os.Create(target + ".sha1.txt")
+	if err != nil {
+		return errors.New("error creating sum file")
+	}
+	defer sumFile.Close()
+
+	content := fmt.Sprintf("%x  %s\n", sum, relativeTarget)
+	_, err = sumFile.WriteString(content)
+	if err != nil {
+		return errors.New("error writing sum file")
+	}
+
+	return nil
+}
+
+func doSHA256(target string) error {
+	file, err := os.Open(target)
+	if err != nil {
+		return errors.New("error opening file")
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return errors.New("error hashing file")
+	}
+
+	// Remove the build directory prefix from the target
+	relativeTarget, err := filepath.Rel(*outDir, target)
+	if err != nil {
+		return errors.New("error getting relative path")
+	}
+
+	sum := hash.Sum(nil)
+
+	// write the sum to a file
+	sumFile, err := os.Create(target + ".sha256.txt")
+	if err != nil {
+		return errors.New("error creating sum file")
+	}
+	defer sumFile.Close()
+
+	content := fmt.Sprintf("%x  %s\n", sum, relativeTarget)
+	_, err = sumFile.WriteString(content)
+	if err != nil {
+		return errors.New("error writing sum file")
+	}
+
+	return nil
+}
+
+func doZip(target string) error {
+	zipFile, err := os.Create(target + ".zip")
+	if err != nil {
+		return errors.New("error creating zip file")
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	fileToZip, err := os.Open(target)
+	if err != nil {
+		return errors.New("error opening file to zip")
+	}
+	defer fileToZip.Close()
+
+	w, err := zipWriter.Create(filepath.Base(target))
+	if err != nil {
+		return errors.New("error creating zip entry")
+	}
+
+	if _, err := io.Copy(w, fileToZip); err != nil {
+		return errors.New("error writing to zip")
+	}
+
+	return nil
 }
 
 func cut(owner, repos string) error {
@@ -172,47 +382,5 @@ func cut(owner, repos string) error {
 		return fmt.Errorf("walking directory: %v", err)
 	}
 
-	return nil
-}
-
-func do(goOS, goARCH, target string) error {
-
-	log.Printf("building %s/%s %s\n", goOS, goARCH, target)
-	err := doBuild(goOS, goARCH, target)
-	if err != nil {
-		return fmt.Errorf("building: %s: %v", target, err)
-	}
-
-	if *sumMD5 {
-		log.Printf("  creating md5sum for %s\n", target)
-		err = doMD5(target)
-		if err != nil {
-			return fmt.Errorf("creating md5: %s: %v", target, err)
-		}
-	}
-
-	if *sumSHA1 {
-		log.Printf("  creating sha1sum for %s\n", target)
-		err = doSHA1(target)
-		if err != nil {
-			return fmt.Errorf("creating sha1: %s: %v", target, err)
-		}
-	}
-
-	if *sumSHA256 {
-		log.Printf("  creating sha256sum for %s\n", target)
-		err = doSHA256(target)
-		if err != nil {
-			return fmt.Errorf("creating sha256: %s: %v", target, err)
-		}
-	}
-
-	if *zipFile {
-		log.Printf("  creating zip for %s\n", target)
-		err = doZip(target)
-		if err != nil {
-			return fmt.Errorf("creating zip: %s: %v", target, err)
-		}
-	}
 	return nil
 }
